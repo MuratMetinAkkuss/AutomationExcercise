f4586f1b675482e5a27cf51f95b0d4717547e1fe
murat metin akkus <metin.akku2017@yandex.com>
2023-07-03 21:15:23 +0200

    training in selenium

--- AutomationExcercise/src/test/java/tests/testCase/TestCase06.java
+++ AutomationExcercise/src/test/java/tests/testCase/TestCase06.java
@@ -3,7 +3,9 @@ package tests.testCase;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
 import java.util.List;
@@ -34,11 +36,23 @@ public class TestCase06 extends TestBase {
         WebElement messageElement = driver.findElement(By.xpath("//textarea[@data-qa='message']"));
         messageElement.sendKeys("Text");
         //7. Upload file
-        //driver.findElement(By.xpath("//input[@name='upload_file']"));
-        //driver.switchTo().activeElement().sendKeys("tests/img/phototestcase06.jpg");
+        WebElement uploadFileElement = driver.findElement(By.xpath("//input[@name='upload_file']"));
+        String filePath = System.getProperty("user.home") + "\\Downloads\\example.png";
+        uploadFileElement.sendKeys(filePath);
+        messageElement.sendKeys(Keys.PAGE_DOWN);
         //8. Click 'Submit' button
+        sleep(1);
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+        sleep(1);
         //9. Click OK button
+        driver.switchTo().alert().accept();
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.PAGE_UP).perform();
+        sleep(1);
         //10. Verify success message 'Success! Your details have been submitted successfully.' is visible
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='status alert alert-success']")).isDisplayed());
         //11. Click 'Home' button and verify that landed to home page successfully
+        driver.findElement(By.xpath("//a[@class='btn btn-success']")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//img[@src='/static/images/home/logo.png']")).isDisplayed());
     }
 }
